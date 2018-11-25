fdc4d5cb5f245e64923d3096a6da23498dc10d6a
Caio Ramos <caioramos97@gmail.com>
2018-11-25 21:59:20 -0200

    testing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/foo')
 def get_foo():
-    return 'VAMO LA'
+    return 'VAMO LAAAAAAAAAAAAAAAAAAAAooooooo'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3001)
